2974ead6135cb784b090a2ab99d2442a6c1ad53b
Trévis Morvany <trevismorvany@gmail.com>
2024-04-14 20:02:56 +0200

    Additions to the ui + bug fixes

--- scripts/fightUI.gd
+++ scripts/fightUI.gd
@@ -4,7 +4,7 @@ var scoreP1
 var scoreP2
 @export var scoreLimit = 15
 
-signal limitReached;
+signal game_end
 
 enum{ PLAYER1, PLAYER2 }
 
@@ -14,18 +14,22 @@ func _ready():
 	set_score(PLAYER2, 0)
 
 func set_score(playerID, score):
-	if(score>=scoreLimit):
-		limitReached.emit();
-	
 	if(playerID == PLAYER1):
 		scoreP1 = score;
 		$ScoreLabelP1.text = str(scoreP1)
+		if scoreP1 >= scoreLimit:
+			$VictoryLabel.text = "Player1 wins"
+			$VictoryLabel.visible = true
+			game_end.emit()
 	elif(playerID == PLAYER2):
 		scoreP2 = score
 		$ScoreLabelP2.text = str(scoreP2)
+		if scoreP2 >= scoreLimit:
+			$VictoryLabel.text = "Player2 wins"
+			$VictoryLabel.visible = true
+			game_end.emit()
 
 func up_score(playerID):
-	
 	if(playerID == PLAYER1):
 		set_score(PLAYER1, scoreP1+1)
 	elif(playerID == PLAYER2):	
@@ -33,6 +37,4 @@ func up_score(playerID):
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	up_score(PLAYER1)
-	up_score(PLAYER2)
 	pass
